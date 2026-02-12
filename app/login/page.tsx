16fd84454458b24dabe198719df1e542d11f0cc6
Jonny Ramirez <jonnyrmzhdz@gmail.com>
2026-02-11 22:58:24 -0600

    feat: implementando dark-mode en plataforma

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2 bg-slate-300">
+    <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
